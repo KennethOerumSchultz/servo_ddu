67fb1f1d5ce556cce004ac7c78fc14437c0398e9
KennethOerumSchultz <55274413+KennethOerumSchultz@users.noreply.github.com>
2025-08-18 11:10:09 +0200

    De første filer til MakeCode-projekt

---
+++ main.ts
@@ -0,0 +1,8 @@
+input.onButtonPressed(Button.A, function () {
+    pins.servoWritePin(AnalogPin.P1, 1)
+})
+input.onButtonPressed(Button.B, function () {
+    pins.servoWritePin(AnalogPin.P1, 40)
+})
+let skift = 0
+pins.servoWritePin(AnalogPin.P1, 1)
