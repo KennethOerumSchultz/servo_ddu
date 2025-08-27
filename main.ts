e8a6e42fd0f5bbafd980db523d6931b1c43a32dd
Jonasjs11 <46404216+Jonasjs11@users.noreply.github.com>
2025-08-27 12:16:26 +0200

    test

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ input.onButtonPressed(Button.A, function () {
     pins.servoWritePin(AnalogPin.P1, 1)
 })
 input.onButtonPressed(Button.B, function () {
-    pins.servoWritePin(AnalogPin.P1, 100)
+    pins.servoWritePin(AnalogPin.P1, 120)
 })
 let skift = 0
 pins.servoWritePin(AnalogPin.P1, 1)
