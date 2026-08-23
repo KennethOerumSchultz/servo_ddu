1e3e51e3e763ca2b5bc69e67454ac03d81ea4df5
KennethOerumSchultz <55274413+KennethOerumSchultz@users.noreply.github.com>
2026-08-23 15:03:15 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ input.onButtonPressed(Button.A, function () {
     pins.servoWritePin(AnalogPin.P1, 1)
 })
 input.onButtonPressed(Button.B, function () {
-    pins.servoWritePin(AnalogPin.P1, 120)
+    pins.servoWritePin(AnalogPin.P1, 125)
 })
 let skift = 0
 pins.servoWritePin(AnalogPin.P1, 1)
